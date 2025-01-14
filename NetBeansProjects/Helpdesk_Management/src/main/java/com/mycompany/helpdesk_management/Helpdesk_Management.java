c3543789090c68839dd6c1a07228552944653831
Ashitha <pallathashitha@gmail.com>
2025-01-14 14:51:37 +0530

    final commit

---
+++ NetBeansProjects/Helpdesk_Management/src/main/java/com/mycompany/helpdesk_management/Helpdesk_Management.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.helpdesk_management;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Helpdesk_Management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
